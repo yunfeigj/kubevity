7c9517f3b63e54a2e11082808588150b382fcfd6
MikelPan <plyx_46204@126.com>
2021-06-26 23:18:51 +0800

    仓库结构调整

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/yunfeigj/kubevity
 
 go 1.15
+
+require (
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
+	github.com/go-logr/logr v0.4.0
+	github.com/gophercloud/gophercloud v0.1.0 // indirect
+	github.com/lithammer/dedent v1.1.0
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/cobra v1.1.3
+	gopkg.in/yaml.v2 v2.4.0
+	helm.sh/helm/v3 v3.6.1
+	k8s.io/api v0.21.2
+	k8s.io/apimachinery v0.21.2
+	k8s.io/cli-runtime v0.21.0
+	k8s.io/client-go v0.21.2
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/kubectl v0.21.0
+	sigs.k8s.io/controller-runtime v0.9.2
+	sigs.k8s.io/structured-merge-diff/v3 v3.0.0 // indirect
+)
---
+++ pkg/util/manager/manager.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2021 The YunFeiGoJi Teams Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package manager
+
+import (
+	kubekeyapiv1alpha1 "github.com/yunfeigj/kubevity/apis/kubevity/v1alpha1"
+	kubekeyclientset "github.com/yunfeigj/kubevity/clients/clientset/versioned"
+	"github.com/yunfeigj/kubevity/pkg/connector"
+	//"github.com/yunfeigj/kubevity/pkg/util/runner"
+	log "github.com/sirupsen/logrus"
+)
+
+// Manager defines all the parameters needed for the installation.
+type Manager struct {
+	ObjName            string
+	Cluster            *kubekeyapiv1alpha1.ClusterSpec
+	Logger             log.FieldLogger
+	Connector          connector.Connector
+	Runner             *runner.Runner
+	AllNodes           []kubekeyapiv1alpha1.HostCfg
+	EtcdNodes          []kubekeyapiv1alpha1.HostCfg
+	MasterNodes        []kubekeyapiv1alpha1.HostCfg
+	WorkerNodes        []kubekeyapiv1alpha1.HostCfg
+	K8sNodes           []kubekeyapiv1alpha1.HostCfg
+	EtcdContainer      bool
+	ClusterHosts       []string
+	WorkDir            string
+	KsEnable           bool
+	KsVersion          string
+	Debug              bool
+	SkipCheck          bool
+	SkipPullImages     bool
+	SourcesDir         string
+	AddImagesRepo      bool
+	InCluster          bool
+	DeployLocalStorage bool
+	Kubeconfig         string
+	Conditions         []kubekeyapiv1alpha1.Condition
+	ClientSet          *kubekeyclientset.Clientset
+	DownloadCommand    func(path, url string) string
+}
+
+// Copy is used to create a copy for Manager.
+func (mgr *Manager) Copy() *Manager {
+	newManager := *mgr
+	return &newManager
+}
